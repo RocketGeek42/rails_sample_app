eca5dbc7a9bd369c497899574d5c8342454df383
jmyork <jen@yorkitconsulting.com>
2019-01-23 22:50:50 +0000

    added html.erb for help and home pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
+  get 'static_pages/home' # maps requests for the URL/static_pages/home to the home action in the StaticPages controller
 
   get 'static_pages/help'
 
